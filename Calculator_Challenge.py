c34cd75af274c307468165d504bb7c98218ae253
Ravi Dubey <38419795+ravi0dubey@users.noreply.github.com>
2022-04-23 22:59:01 +0000

    Calculator

--- Calculator_Challenge.py
+++ Calculator_Challenge.py
@@ -1,43 +1,51 @@
 import clear
-#from replit import clear
 
-def calc(num1, num2, opr):
-  if(opr == "*"):
-    return num1 * num2
-  elif(opr == "+"):
-    return num1 + num2
-  elif(opr == "-"):
-    return num1 - num2
-  elif(opr == "/"):
-    return num1 / num2
-  else:
-    return(0)
-    
+
+def add(num1,num2):
+  return num1 + num2
+def subtract(num1,num2):
+  return num1 - num2
+def multiply(num1,num2):
+  return num1 * num2 
+def divide(num1,num2):
+  return num1 / num2
+
+#Dictionary for Operations
+operations = {
+  "+":add,
+  "-":subtract,
+  "*":multiply,
+  "/":divide
+  }    
+
 #main portion of code 
 
-invalid_opr= False    
+ 
 loop_cal = "y"
 
 while (loop_cal !='n'): 
   num1= int(input("What's the first number?: "))
-  print("+\n-\n*\n/") 
+  for oper in operations:
+   print(oper)
+  invalid_opr= False   
   while not(invalid_opr):
-    opr = input("Pick an Operation: ") 
-    if( opr not in ("*","/","+","-")):
+    oper = input("Pick an Operation: ") 
+    if(oper not in ("*","/","+","-")):
       invalid_opr= False
-      print("Enter Valid Operator")
+      print("Enter Valid Operator from above list")
     else:
       invalid_opr= True   
-  num2= int(input("What's the next number?: "))
   
-  if(opr == "/" and (num1==0 or num2==0)):
-    print("Enter Valid number inputs")
-  else:     
-    result = calc(num1, num2, opr)
-    print(f"{num1} {opr} {num2}"" = " f"{result} ")
-    loop_cal = input(f"Type 'y' to continue calculating with{result} "," or type 'n' to start a new calculation: ").lower()
-    if(loop_cal =="y" ):
-      num1 == result
-    else:
-      clear()
+  cal_function = operations[oper]
+  print(cal_function)
+  num2= int(input("What's the next number?: "))
+  result = cal_function(num1,num2)
+
+  print(f"{num1} {oper} {num2}"" = " f"{result} ")
+  # loop_cal = input(f"Type 'y' to continue calculating with{result} "," or type 'n' to start a new calculation: ")
+  
+  # if(loop_cal =="y" ):
+  #    num1 == result
+  # else:
+  #   clear()
   

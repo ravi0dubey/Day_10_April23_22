11372693185c0a09d41dbede1009717f917c9f88
Ravi Dubey <38419795+ravi0dubey@users.noreply.github.com>
2022-04-23 22:27:16 +0000

    Calculator

--- Calculator_Challenge.py
+++ Calculator_Challenge.py
@@ -1,7 +1,43 @@
-def outer_function(a, b):
-    def inner_function(c, d):
-        return c + d
-    return inner_function(a, b)
- 
-result = outer_function(5, 10)
-print(result)
+import clear
+#from replit import clear
+
+def calc(num1, num2, opr):
+  if(opr == "*"):
+    return num1 * num2
+  elif(opr == "+"):
+    return num1 + num2
+  elif(opr == "-"):
+    return num1 - num2
+  elif(opr == "/"):
+    return num1 / num2
+  else:
+    return(0)
+    
+#main portion of code 
+
+invalid_opr= False    
+loop_cal = "y"
+
+while (loop_cal !='n'): 
+  num1= int(input("What's the first number?: "))
+  print("+\n-\n*\n/") 
+  while not(invalid_opr):
+    opr = input("Pick an Operation: ") 
+    if( opr not in ("*","/","+","-")):
+      invalid_opr= False
+      print("Enter Valid Operator")
+    else:
+      invalid_opr= True   
+  num2= int(input("What's the next number?: "))
+  
+  if(opr == "/" and (num1==0 or num2==0)):
+    print("Enter Valid number inputs")
+  else:     
+    result = calc(num1, num2, opr)
+    print(f"{num1} {opr} {num2}"" = " f"{result} ")
+    loop_cal = input(f"Type 'y' to continue calculating with{result} "," or type 'n' to start a new calculation: ").lower()
+    if(loop_cal =="y" ):
+      num1 == result
+    else:
+      clear()
+  
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import clear
 #from replit import clear
 
 def calc(num1, num2, opr):
-
   if(opr == "*"):
     return num1 * num2
   elif(opr == "+"):
@@ -14,15 +13,14 @@ def calc(num1, num2, opr):
   else:
     return(0)
     
-invalid_opr= False    
+#main portion of code 
 
+invalid_opr= False    
 loop_cal = "y"
 
-while (loop_cal !='n'):
-  
+while (loop_cal !='n'): 
   num1= int(input("What's the first number?: "))
-  print("+\n-\n*\n/")
-  
+  print("+\n-\n*\n/") 
   while not(invalid_opr):
     opr = input("Pick an Operation: ") 
     if( opr not in ("*","/","+","-")):
@@ -30,19 +28,16 @@ while (loop_cal !='n'):
       print("Enter Valid Operator")
     else:
       invalid_opr= True   
-  
   num2= int(input("What's the next number?: "))
+  
   if(opr == "/" and (num1==0 or num2==0)):
-    print("Enter Valid input")
-  
-  
-  
-  
-  result = calc(num1, num2, opr)
-  print(f"{num1} {opr} {num2}"" = " f"{result} ")
-  loop_cal = input(f"Type 'y' to continue calculating with{result} "," or type 'n' to start a new calculation: ").lower()
-  if(loop_cal =="y" ):
-    num1 == result
-  else:
-    clear()
+    print("Enter Valid number inputs")
+  else:     
+    result = calc(num1, num2, opr)
+    print(f"{num1} {opr} {num2}"" = " f"{result} ")
+    loop_cal = input(f"Type 'y' to continue calculating with{result} "," or type 'n' to start a new calculation: ").lower()
+    if(loop_cal =="y" ):
+      num1 == result
+    else:
+      clear()
   

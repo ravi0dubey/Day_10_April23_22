64d94a1fe1391966b1c92e569a97aca03328bccd
Ravi Dubey <38419795+ravi0dubey@users.noreply.github.com>
2022-04-23 22:19:19 +0000

    Initial changes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import clear
+#from replit import clear
+
 def calc(num1, num2, opr):
 
   if(opr == "*"):
@@ -14,30 +16,33 @@ def calc(num1, num2, opr):
     
 invalid_opr= False    
 
+loop_cal = "y"
 
-num1= int(input("What's the first number?: "))
-print("+\n-\n*\n/")
-
-while not(invalid_opr):
-  opr = input("Pick an Operation: ") 
-  if( opr not in ("*","/","+","-")):
-    invalid_opr= False
-    print("Enter Valid Operator")
+while (loop_cal !='n'):
+  
+  num1= int(input("What's the first number?: "))
+  print("+\n-\n*\n/")
+  
+  while not(invalid_opr):
+    opr = input("Pick an Operation: ") 
+    if( opr not in ("*","/","+","-")):
+      invalid_opr= False
+      print("Enter Valid Operator")
+    else:
+      invalid_opr= True   
+  
+  num2= int(input("What's the next number?: "))
+  if(opr == "/" and (num1==0 or num2==0)):
+    print("Enter Valid input")
+  
+  
+  
+  
+  result = calc(num1, num2, opr)
+  print(f"{num1} {opr} {num2}"" = " f"{result} ")
+  loop_cal = input(f"Type 'y' to continue calculating with{result} "," or type 'n' to start a new calculation: ").lower()
+  if(loop_cal =="y" ):
+    num1 == result
   else:
-    invalid_opr= True   
-
-num2= int(input("What's the next number?: "))
-if(opr == "/" and (num1==0 or num2==0)):
-  print("Enter Valid input")
-
-
-
-
-result = calc(num1, num2, opr)
-print(f"{num1} {opr} {num2}"" = " f"{result} ")
-loop_cal = input(f"Type 'y' to continue calculating with{result} "," or type 'n' to start a new calculation: ").lower()
-if(loop_cal =="y" ):
-  num1 == result
-else:
-  clear()
-
+    clear()
+  

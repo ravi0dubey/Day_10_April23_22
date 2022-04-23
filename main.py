99fe4289823b209595e3e3723793b3501a083812
Ravi Dubey <38419795+ravi0dubey@users.noreply.github.com>
2022-04-23 23:25:02 +0000

    Calculator

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from art import logo
 import clear
-
 
 def add(num1,num2):
   return num1 + num2
@@ -20,32 +20,37 @@ operations = {
 
 #main portion of code 
 
- 
-loop_cal = "y"
-
-while (loop_cal !='n'): 
-  num1= int(input("What's the first number?: "))
-  for oper in operations:
-   print(oper)
-  invalid_opr= False   
-  while not(invalid_opr):
-    oper = input("Pick an Operation: ") 
-    if(oper not in ("*","/","+","-")):
-      invalid_opr= False
-      print("Enter Valid Operator from above list")
+def calculator():
+  print(logo)
+  loop_cal = " "
+  
+  while (loop_cal !='n'): 
+    if(loop_cal !="y" ):
+      num1= int(input("What's the first number?: "))
+    for oper in operations:
+     print(oper)
+    invalid_opr= False   
+    while not(invalid_opr):
+      oper = input("Pick an Operation: ") 
+      if(oper not in ("*","/","+","-")):
+        invalid_opr= False
+        print("Enter Valid Operator from above list")
+      else:
+        invalid_opr= True   
+    
+    cal_function = operations[oper]
+    num2= int(input("What's the next number?: "))
+    result = cal_function(num1,num2)
+  
+    print(f"{num1} {oper} {num2}"" = " f"{result} ")
+  
+    print(f"Type 'y' to continue calculating with {result}"," or type 'n' to start a new calculation: \n")
+    loop_cal = input()
+    
+    if(loop_cal =="y" ):
+       num1 = result
     else:
-      invalid_opr= True   
+      calculator()
   
-  cal_function = operations[oper]
-  print(cal_function)
-  num2= int(input("What's the next number?: "))
-  result = cal_function(num1,num2)
-
-  print(f"{num1} {oper} {num2}"" = " f"{result} ")
-  # loop_cal = input(f"Type 'y' to continue calculating with{result} "," or type 'n' to start a new calculation: ")
-  
-  # if(loop_cal =="y" ):
-  #    num1 == result
-  # else:
-  #   clear()
-  
+#call Calculator function
+calculator()
